4b0e4bcb61d2107dc72c84e93ececb5e36febe0f
Patrick Hanna <patrickjh1998@hotmail.com>
2020-01-25 14:14:28 -0500

    updates

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 
-const http = require('http');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-const server = http.createServer((request, response) => {
-    response.end("Patrick is an awesome programmer!");
+app.use(express.json())
+
+const usersRouter = express.Router();
+
+usersRouter.get('/', (request, response) => {
+    response.send("you want to retreive all users");
 });
 
-const port = process.env.PORT || 5000;
+usersRouter.get('/:id', (request, response) => {
+    const id = request.params.id;
+    response.send("you want to retrieve a user with the id of " + id);
+});
 
-server.listen(port, () => console.log(`the server is running on port ${port}!!`));
+usersRouter.post ('/create', (request, response) => {
+    response.send(request.body);
+});
+
+app.use('/product-items', usersRouter);
+
+const port = process.env.PORT || 5000;
+console.log(process.env);
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
 
