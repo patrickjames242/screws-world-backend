15ad95ccfccf952e4ff626521e8fc2362c91fd0d
Patrick Hanna <patrickjh1998@hotmail.com>
2020-01-26 01:15:41 -0500

    updates

--- index.js
+++ index.js
@@ -1,38 +1,94 @@
 
 const express = require('express');
 const { Client } = require('pg');
+
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+
+client.connect()
+
+
+function SuccessResponse(data) {
+    return {
+        status: 'success',
+        data: data,
+    }
+}
+
+function FailureResponse(errorMessage) {
+    return {
+        status: 'failure',
+        errorMessage: errorMessage,
+    }
+}
+
+
 const app = express();
 
+app.use(express.json());
 
+const categoriesRouter = express.Router();
 
-// const client = new Client();
+const serverErrorCode = 500;
+const badRequestCode = 400;
 
-// client.connect()
-// .then(function(){
-//     console.log(arguments);
-// });
-
-
-
-app.use(express.json())
-
-const usersRouter = express.Router();
-
-usersRouter.get('/', (request, response) => {
-    // response.send("you want to retreive all users");
-    response.json(process.env);
+categoriesRouter.get('/', (_, response) => {
+    client.query('select * from product_category')
+        .then(({rows}) => {
+            response.json(SuccessResponse(rows))
+        }).catch((error) => {
+            response.status(serverErrorCode).json(FailureResponse(error.message));
+        });
 });
 
-usersRouter.get('/:id', (request, response) => {
-    const id = request.params.id;
-    response.send("you want to retrieve a user with the id of " + id);
+categoriesRouter.get('/:id', (request, response) => {
+    const id = Number(request.params.id);
+    if (isNaN(id) === true) {
+        response.status(badRequestCode).json(FailureResponse(`the id of '${request.params.id}' is invalid`));
+        return;
+    }
+    client.query(`select * from product_category where id = ${id}`)
+        .then(({ rows }) => {
+            const obj = rows[0];
+            if (obj) {
+                response.json(SuccessResponse(obj));
+            } else {
+                response.status(badRequestCode).json(FailureResponse(`could not find a category with an id of ${id}`));
+            }
+        }).catch((error) => {
+            response.status(serverErrorCode).json(FailureResponse(error.message));
+        });
 });
 
-usersRouter.post ('/create', (request, response) => {
-    response.send(request.body);
+categoriesRouter.post('/create', (request, response) => {
+    const props = request.body;
+
+    if (props.title == undefined) {
+        response.status(badRequestCode).json(FailureResponse("The 'title' property is required, but you have not included it in the body."));
+        return;
+    }
+    const propsToUse = [
+        { key: "title", value: props.title },
+        { key: "description", value: props.description },
+        { key: "parent_category", vaue: props.parent_category },
+    ].filter(x => x.value != undefined);
+
+    const propNamesString = `(${propsToUse.map(x => x.key).join(", ")})`;
+    const valuesString = `(${propsToUse.map((_, i) => "$" + (i + 1)).join(", ")})`;
+    const values = propsToUse.map(x => x.value);
+
+    client.query(`insert into product_category ${propNamesString} values ${valuesString} returning *`, values)
+        .then(({rows}) => {
+            response.json(SuccessResponse(rows[0]));
+        }).catch((error) => {
+            response.status(serverErrorCode).json(FailureResponse(error.message));
+        });
 });
 
-app.use('/product-items', usersRouter);
+app.use('/categories', categoriesRouter);
 
 const port = process.env.PORT || 5000;
 
